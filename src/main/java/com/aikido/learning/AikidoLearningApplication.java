e74f887a23b6ea0afc78b6d0525cd402f421da0d
J-sicard <ptiptilu@hotmail.fr>
2024-08-07 15:01:12 +0200

    first commit

---
+++ src/main/java/com/aikido/learning/AikidoLearningApplication.java
@@ -0,0 +1,13 @@
+package com.aikido.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AikidoLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AikidoLearningApplication.class, args);
+	}
+
+}
